6af25a9a64e1a248acaa7efded24ad4782236a80
Vitor Henrique Percincula Pinheiro <36556383+HenriquePercincula@users.noreply.github.com>
2020-11-22 16:08:59 -0300

    Add files via upload

---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "bd-si" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
---
+++ src/Handler/Home.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Home where
+
+import Import
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do 
+    [whamlet|
+            <h1>
+                <a href=@{ProdutoR}>
+                    CADASTRO DE PRODUTO
+    |]
