1cf5233f71668c9376c71702542515c4cbbaad65
Vitor Henrique Percincula Pinheiro <36556383+HenriquePercincula@users.noreply.github.com>
2020-11-26 00:26:59 -0300

    Add files via upload

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,13 +9,21 @@ module Handler.Home where
 import Import
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout $ do 
+getHomeR = defaultLayout $ do
+    addStylesheet (StaticR css_estilo_css)
     toWidgetHead [hamlet|<script src=@{StaticR js_ola_js}>|]
     [whamlet|
             <h1>
                 SISTEMA DE PRODUTOS
             <br>
             <img src=@{StaticR imgs_shop_svg}>
-            <a href=@{ProdutoR}>
-                CADASTRO DE PRODUTO
+            
+            <ul>
+                <li>
+                    <a href=@{ProdutoR}>
+                        CADASTRO DE PRODUTO
+                <li>
+                    <a href=@{ListaR}>
+                        LISTAGEM DE PRODUTO
+                
     |]
--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -9,37 +9,39 @@ module Handler.Produto where
 import Import
 import Text.Lucius
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-
 -- fmap functor f => (a -> b) -> f a -> f b
 -- (<*>) :: Applicative f => f (a -> b) -> f a -> f b
--- renderDivs
-
-formProduto :: Form Produto
-formProduto = renderBootstrap3 BootstrapBasicForm $ Produto -- ou renderDivs
+formProduto :: Maybe Produto -> Form Produto
+formProduto mp = renderBootstrap3 BootstrapBasicForm{- ou renderDivs-} $ Produto 
     <$> areq textField (FieldSettings  "Nome: " 
                                                 Nothing
                                                 (Just "h21")
                                                 Nothing
-                                                [("class","minhaClasse")]) Nothing 
-    <*> areq doubleField "Preco: " Nothing
+                                                [("class","minhaClasse")]) 
+                                                (fmap produtoNome mp) 
+    <*> areq doubleField "Preco: " (fmap produtoValor mp) 
  
-getProdutoR :: Handler Html
-getProdutoR = do 
+auxProdutoR :: Route App -> Maybe Produto -> Handler Html
+auxProdutoR rt mp = do
     -- formWidget TERA TODAS AS TAGS DE FORMULARIO
-    (formWidget, _) <- generateFormPost formProduto
+    (formWidget, _) <- generateFormPost (formProduto mp)
     defaultLayout $ do
         -- pasta: static/css/bootstrap.css
         addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_estilo_css)
         toWidgetHead $(luciusFile "templates/descr.lucius")
         [whamlet|
-            <form action=@{ProdutoR} method=post>
+            <form action=@{rt} method=post>
                 ^{formWidget}
                 <input type="submit" value="OK">
         |]
 
+getProdutoR :: Handler Html
+getProdutoR = auxProdutoR ProdutoR Nothing
+
 postProdutoR :: Handler Html
 postProdutoR = do
-    ((result, _), _) <- runFormPost formProduto
+    ((result, _), _) <- runFormPost (formProduto Nothing)
     case result of
         FormSuccess produto -> do
             pid <- runDB $ insert produto
@@ -51,7 +53,7 @@ getDescR pid = do
     produto <- runDB $ get404 pid
     defaultLayout $ do
         $(whamletFile "templates/descr.hamlet")
-{-
+
 getListaR :: Handler Html
 getListaR = do
     -- Handler [Entity ProdutoId Produto]
@@ -59,4 +61,17 @@ getListaR = do
     defaultLayout $ do
         $(whamletFile "templates/listar.hamlet")
 
-        -}
+getUpdProdR :: ProdutoId -> Handler Html
+getUpdProdR pid = do
+    antigo <- runDB $ get404 pid
+    auxProdutoR (UpdProdR pid) (Just antigo)
+
+-- update from produto set... where id = pid
+postUpdProdR :: ProdutoId -> Handler Html
+postUpdProdR pid = do
+    ((result, _), _) <- runFormPost (formProduto Nothing)
+    case result of
+        FormSuccess produto -> do
+            runDB $ replace pid produto
+            redirect ListaR
+        _-> redirect HomeR
